827bea66e8b9c81fddb5f790a5c825cb44ce2451
Hoa Tran <hoaqt91@gmail.com>
2020-12-05 23:52:19 +0700

    Introduce dummy sms service.

---
+++ dummy-sms/src/main/java/au/com/nab/dummysms/DummySmsApplication.java
@@ -0,0 +1,13 @@
+package au.com.nab.dummysms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummySmsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DummySmsApplication.class, args);
+  }
+
+}
---
+++ dummy-sms/src/main/java/au/com/nab/dummysms/dto/SmsRequest.java
@@ -0,0 +1,20 @@
+package au.com.nab.dummysms.dto;
+
+public class SmsRequest {
+  private String message;
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  @Override
+  public String toString() {
+    return "SmsRequest{" +
+        "message='" + message + '\'' +
+        '}';
+  }
+}
